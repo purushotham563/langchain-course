0d699e5a6578c7f6faabfbe96c40de4c1c84ab3b
Purushotham Reddy <purushotham.appireddy@gmail.com>
2026-08-11 16:08:16 +0530

    Added the pydantic structure output to an ai agent

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ from langchain.tools import tool
 from langchain.messages import HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from tavily import TavilyClient
+from typing import List
+from pydantic import BaseModel,Field
 
 tavily=TavilyClient()
 
 load_dotenv()
+
+
+class Source(BaseModel):
+    '''Schema for a source used by the agent'''
+    url:str=Field(description="The URL for the source")
+
+class AgentResponse(BaseModel):
+    '''Schema for the agent response with answer and source'''
+    answer:str=Field(description="The agent answer to the query")
+    sources:List[Source]=Field(default_factory=list,description="List of source used to generate the answer")
+
 
 @tool
 def search(query:str)->str:
@@ -21,7 +34,7 @@ def search(query:str)->str:
     return tavily.search(query=query)
 llm = ChatGoogleGenerativeAI(model="gemini-3.5-flash-lite")
 tools=[search]
-agent=create_agent(model=llm,tools=tools)
+agent=create_agent(model=llm,tools=tools,response_format=AgentResponse)
 
 def main():
     print("Hello from langchain course")
