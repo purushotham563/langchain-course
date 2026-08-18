be63f5ce2cbcc58eba5c02f4d0ef0381c4abd62c
Purushotham Reddy <purushotham.appireddy@gmail.com>
2026-08-18 16:39:22 +0530

    simple yet effective react agent using langraph

---
+++ nodes.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from langgraph.graph import MessagesState
+from langgraph.prebuilt import ToolNode
+from react import llm,tools
+load_dotenv()
+
+SYSTEM_MESSAGE="""
+You are helpful assistant that can use tools to answer question
+"""
+def run_agent_reasoning(state:MessagesState)->MessagesState:
+    """Run the agnet reasoning node"""
+    response=llm.invoke([{"role":"system","content":SYSTEM_MESSAGE},*state["messages"]])
+    return {"messages":[response]}
+tool_node=ToolNode(tools)
+
+
